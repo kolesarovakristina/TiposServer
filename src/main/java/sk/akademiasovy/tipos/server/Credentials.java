d97a7d9e7077845cfd1745c27fd14cdb9e081c67
kristina kolesarova <kristina.kolesarova@akademiasovy.sk>
2018-03-05 22:10:56 +0100

    Initial commit

---
+++ src/main/java/sk/akademiasovy/tipos/server/Credentials.java
@@ -0,0 +1,21 @@
+package sk.akademiasovy.tipos.server;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+public class Credentials {
+    @JsonProperty("username")
+    public String username;
+
+    @JsonProperty("password")
+    public String password;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
